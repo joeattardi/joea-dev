530853bb407955447141a2f1a2c9aeb73854549f
Joe Attardi <jattardi@gmail.com>
2025-11-21 09:10:46 -0500

    Remove highlighting and sidebars for now

--- public/highlight-init.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  try {
-    var h = window['hljs'];
-    if (h && typeof h.highlightAll === 'function') h.highlightAll();
-  } catch (e) {
-    // fail silently
-  }
-});
